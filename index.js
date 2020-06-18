d61a1f8d49800dd5e24c0f3aa9a3a889053f474e
PRAMESH <Parameswarkarki@gmail.com>
2020-06-18 18:46:43 +0545

    Initial Commit

---
+++ index.js
@@ -0,0 +1,68 @@
+function library(bookName, authorName, category) {
+  this.bookName = bookName;
+  this.authorName = authorName;
+  this.category = category;
+}
+
+function Display() {}
+
+let myForm = document.getElementById("libraryForm");
+myForm.addEventListener("submit", libraryFormSubmit);
+
+Display.prototype.add = function (book) {
+  let tableBody = document.getElementById("tableContent");
+  let templateString = `
+                          <tr>
+                          <td>${book.bookName}</td>
+                          <td>${book.authorName}</td>
+                          <td>${book.category}</td>
+                          </tr>`;
+  tableBody.innerHTML += templateString;
+};
+
+Display.prototype.clear = function () {
+  let myForm = document.getElementById("libraryForm");
+  myForm.reset();
+};
+
+Display.prototype.validation = function (book) {
+  if (book.bookName.length < 3 && book.authorName.length < 4) return false;
+  else return true;
+};
+
+Display.prototype.show = function (type, message) {
+  let myMessageDiv = document.getElementById("message");
+  myMessageDiv.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+  <strong>Message!</strong> ${message}
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>`;
+  setTimeout(() => {
+    myMessageDiv.innerHTML = "";
+  }, 3000);
+};
+
+let display = new Display();
+function libraryFormSubmit(e) {
+  e.preventDefault();
+  let bookName = document.getElementById("bookName").value;
+  let authorName = document.getElementById("authorName").value;
+  let programmingBooks = document.getElementById("programmingBooks");
+  let history = document.getElementById("history");
+  let other= document.getElementById("other");
+
+  if (programmingBooks.checked) category = programmingBooks.value;
+  else if (history.checked) category = history.value;
+  else if (other.checked) category = other.value;
+
+  let book = new library(bookName, authorName, category);
+
+  if (display.validation(book)) {
+    display.add(book);
+    display.clear();
+    display.show("success", "Your Book has been sucessfully added");
+  } else {
+    display.show("danger", "This book cannt Be added");
+  }
+}
